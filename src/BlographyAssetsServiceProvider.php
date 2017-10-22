dca6078a6784082d212a68575c4503b8a2fd6530
Josh 'VaMoose' harington <josh1@live.co.za>
2017-10-22 15:04:24 +0200

    Assets

---
+++ src/BlographyAssetsServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 3ncode
+ * Date: 2017/10/22
+ * Time: 14:59
+ */
+
+namespace _3ncode\Assets;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class BlographyAssetsServiceProvider extends ServiceProvider {
+
+    public function boot() {
+        require __DIR__ . '/../vendor/autoload.php';
+    }
+
+    public function register() {
+
+        try {
+            rename(base_path('public/assets'), base_path('public/assets-original'));
+        } catch(\Exception $e) {
+
+        }
+
+        $this->publishes([
+            __DIR__.'/public/assets' => base_path('public/assets')
+        ]);
+    }
+
+}
